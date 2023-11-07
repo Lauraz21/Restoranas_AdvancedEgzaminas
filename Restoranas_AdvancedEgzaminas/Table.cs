fa8ff9e761fdf4cf16f671e2c9eb0a1db2740a8d
Laura <Laura@DESKTOP-BESSB6E>
2023-11-07 20:47:00 +0200

    Add project files.

---
+++ Restoranas_AdvancedEgzaminas/Table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoranas_AdvancedEgzaminas
+{
+    public class Table
+    {
+        public int ID { get; set; }
+        public List<Dish> OrderDishes { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public bool IsReserved { get; set; }
+
+        public void ReserveTable()
+        {
+            ReservationTime = DateTime.Now;
+            IsReserved = true;
+            OrderDishes = new List<Dish>();
+        }
+        public void FreeTable()
+        {
+            IsReserved = false;
+        }
+        public double GetOrderTotal()
+        {
+            return OrderDishes.Sum(dish => dish.Price);
+        }
+    }
+}
